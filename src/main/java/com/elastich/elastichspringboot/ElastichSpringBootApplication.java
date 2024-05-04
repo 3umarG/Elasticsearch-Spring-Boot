85277841950e6a88402182be1d4a4c18510d3504
OmarGomaaFCi <omargomaafcitu@gmail.com>
2024-05-04 14:54:21 +0200

    Add initial

---
+++ src/main/java/com/elastich/elastichspringboot/ElastichSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.elastich.elastichspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ElastichSpringBootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ElastichSpringBootApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/elastich/elastichspringboot/ElastichSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.elastich.elastichspringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ElastichSpringBootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
