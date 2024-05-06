5472d844743b391e70c655c86e311a3e64eef2e7
OmarGomaaFCi <omargomaafcitu@gmail.com>
2024-05-06 19:39:23 +0200

    implement sorting

--- src/main/java/com/elastich/elastichspringboot/DAO/CriteriaQueryRepository.java
+++ src/main/java/com/elastich/elastichspringboot/DAO/CriteriaQueryRepository.java
@@ -3,6 +3,7 @@ package com.elastich.elastichspringboot.DAO;
 import com.elastich.elastichspringboot.model.Book;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
 import org.springframework.data.elasticsearch.core.ElasticsearchOperations;
 import org.springframework.data.elasticsearch.core.SearchHit;
 import org.springframework.data.elasticsearch.core.query.Criteria;
@@ -31,7 +32,7 @@ public class CriteriaQueryRepository {
      *                   "fields":[
      *                      "title"
      *                   ],
-     *                   "query":"*b*"
+     *                   "query":"*Book*"
      *                }
      *             },
      *             {
@@ -40,20 +41,31 @@ public class CriteriaQueryRepository {
      *                   "fields":[
      *                      "author"
      *                   ],
-     *                   "query":"*b*"
+     *                   "query":"*Book*"
      *                }
      *             }
      *          ]
      *       }
      *    },
-     *    "size":2
+     *    "size":3,
+     *    "sort":[
+     *       {
+     *          "price":{
+     *             "mode":"min",
+     *             "order":"desc"
+     *          }
+     *       }
+     *    ]
      * }
      */
     public List<SearchHit<Book>> searchByTitle(String q, int page, int size) {
         Criteria criteria =
                 new Criteria("title").contains(q)
                         .or("author").contains(q);
-        Query searchQuery = new CriteriaQuery(criteria, PageRequest.of(page, size));
+
+        Query searchQuery = new CriteriaQuery(criteria, PageRequest.of(page, size))
+                .addSort(Sort.by(Sort.Direction.DESC, "price"));
+
         return elasticsearchOperations
                 .search(searchQuery, Book.class)
                 .stream().toList();
